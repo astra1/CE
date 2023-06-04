6e647229cd9161d3f51f7e53d379f8744d0cafe1
Maxim Cheremkhin <astralfestival@gmail.com>
2023-06-04 23:38:52 +0300

    feat(auth-session-storage-service): replaced cryptojs with native implementation

--- webapp/src/app/core/services/auth-session-storage.service.ts
+++ webapp/src/app/core/services/auth-session-storage.service.ts
@@ -3,9 +3,9 @@
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); You may not use
  * this file except in compliance with the License. A copy of the License is located at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * or in the "license" file accompanying this file. This file is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or
  * implied. See the License for the specific language governing permissions and
@@ -13,33 +13,28 @@
  */
 
 import { Injectable } from '@angular/core';
+import { LoggerService } from 'src/app/shared/services/logger.service';
 import { DataCacheService } from './data-cache.service';
-import * as CryptoJS from 'crypto-js';
-import { LoggerService } from '../../shared/services/logger.service';
-
 
 @Injectable()
 export class AuthSessionStorageService {
-  constructor(private dataStore: DataCacheService,
-              private loggerService: LoggerService) {}
+    constructor(private dataStore: DataCacheService, private loggerService: LoggerService) {}
 
-  saveUserDetails(fetchedResult) {
-    this.dataStore.setCurrentUserLoginDetails(JSON.stringify(fetchedResult));
-    this.dataStore.setUserDefaultAssetGroup(fetchedResult.userInfo.defaultAssetGroup);
+    saveUserDetails(fetchedResult) {
+        this.dataStore.setCurrentUserLoginDetails(JSON.stringify(fetchedResult));
+        this.dataStore.setUserDefaultAssetGroup(fetchedResult.userInfo.defaultAssetGroup);
 
-    let email = this.dataStore.getUserDetailsValue() ? this.dataStore.getUserDetailsValue().getEmail() : 'guestUser';
+        const email = this.dataStore.getUserDetailsValue().getEmail() || 'guestUser';
 
-    if (!email) {
-      email = 'guestUser';
+        crypto.subtle.digest('SHA-256', new TextEncoder().encode(email)).then((res) => {
+            const hashedArray = Array.from(new Uint8Array(res));
+            const hashedEmail = hashedArray.map((b) => b.toString(16).padStart(2, '0')).join('');
+            this.dataStore.setHashedIdOfUser(hashedEmail);
+
+            // Below local storage key is added and removed to
+            // transfer storage to other tabs on successful login */
+            localStorage.setItem('loginsuccess', '.');
+            localStorage.removeItem('loginsuccess');
+        });
     }
-    const emailHashed = (CryptoJS.SHA256(email)).toString();
-    this.dataStore.setHashedIdOfUser(emailHashed);
-
-    /* Below local storage key is added and removed to
-    transfer storage to other tabs on successful login */
-    localStorage.setItem('loginsuccess', '.');
-    localStorage.removeItem('loginsuccess');
-  }
-
-
 }
